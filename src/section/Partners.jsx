153f2a0df095ebf32f91a78c25099ddfa286ab86
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-07-07 03:34:11 +0530

    Update Partners.jsx

---
+++ src/section/Partners.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Partners() {
+  return (
+    <section>
+        <div>
+            <h1>PARTNERS</h1>
+        </div>
+        
+    </section>
+  )
+}
+
+export default Partners
